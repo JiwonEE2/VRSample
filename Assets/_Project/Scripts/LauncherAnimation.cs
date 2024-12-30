cbd72219fa97d668bb2fbcd5d3bbc254e6cecd5c
JiwonEE2 <jiwonchichi1@gmail.com>
2024-12-30 16:47:09 +0900

    launcher 모델로 변경 및 트리거 눌리도록 구현

---
+++ Assets/_Project/Scripts/LauncherAnimation.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class LauncherAnimation : MonoBehaviour
+{
+	public Transform trigger;
+	private Vector3 startTriggerPos;
+	private Vector3 animTriggerPos;
+	private ActionBasedController ABC;
+
+	private void Awake()
+	{
+		startTriggerPos = trigger.localPosition;
+		animTriggerPos = trigger.localPosition;
+		animTriggerPos.z = 0.01f;
+		ABC = GetComponentInParent<ActionBasedController>();
+	}
+
+	private void OnEnable()
+	{
+		ABC.selectAction.reference.action.performed += TriggerAction;
+		ABC.selectAction.reference.action.canceled += TriggerAction;
+	}
+
+	private void OnDisable()
+	{
+		ABC.selectAction.reference.action.performed -= TriggerAction;
+		ABC.selectAction.reference.action.canceled -= TriggerAction;
+	}
+
+	private void TriggerAction(InputAction.CallbackContext context)
+	{
+		if (context.performed)
+		{
+			trigger.localPosition = animTriggerPos;
+		}
+		else
+		{
+			trigger.localPosition = startTriggerPos;
+		}
+	}
+}
